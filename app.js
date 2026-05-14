5816cba5d238545b4537ecc7d43ddfff55cb60f6
abdelherrera303-creator <abdelherrera303@gmail.com>
2026-05-14 19:22:09 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,30 @@
-function hablar(){
-alert("KRONOS activado...");
+const chat = document.getElementById("chat");
+
+function enviar(){
+
+const input = document.getElementById("mensaje");
+const texto = input.value;
+
+chat.innerHTML += "<p><b>Tú:</b> "+texto+"</p>";
+
+respuestaKronos(texto);
+
+input.value="";
+}
+
+function respuestaKronos(texto){
+
+let respuesta = "Estoy aprendiendo...";
+
+if(texto.includes("hola")){
+respuesta="Hola. Soy KRONOS.";
+}
+
+if(texto.includes("quien eres")){
+respuesta="Soy tu IA personal.";
+}
+
+chat.innerHTML += "<p><b>Kronos:</b> "+respuesta+"</p>";
+
+chat.scrollTop = chat.scrollHeight;
 }
